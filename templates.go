d0f041b7c5223602f5786c21f9cddcc243ba7058
Peter Li <71104212+zli3006@users.noreply.github.com>
2024-11-06 16:20:48 +0000

    basic functionality for event creation

--- templates.go
+++ templates.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"html/template"
+    "html/template"
 )
 
 var tmpl = make(map[string]*template.Template)
 
 func init() {
-	m := template.Must
-	p := template.ParseFiles
-	tmpl["index"] = m(p("templates/index.gohtml", "templates/layout.gohtml"))
+    m := template.Must
+    p := template.ParseFiles
+    tmpl["index"] = m(p("templates/index.gohtml", "templates/layout.gohtml"))
+    tmpl["new_event"] = m(p("templates/new_event.gohtml", "templates/layout.gohtml"))
 }
