8c2f6f39548d11d50abca755742698e8808729f0
Jaryd Raizon <jraizon@gmail.com>
2024-11-20 03:29:16 +0000

    Add About Us page

--- templates.go
+++ templates.go
@@ -9,9 +9,12 @@ var tmpl = make(map[string]*template.Template)
 func init() {
 	m := template.Must
 	p := template.ParseFiles
+
+	// Parsing templates and adding them to the map
 	tmpl["index"] = m(p("templates/index.gohtml", "templates/layout.gohtml"))
 	tmpl["new_event"] = m(p("templates/new_event.gohtml", "templates/layout.gohtml"))
 	tmpl["event_detail"] = m(p("templates/event_detail.gohtml", "templates/layout.gohtml"))
 	tmpl["login"] = m(p("templates/login.gohtml", "templates/layout.gohtml"))
 	tmpl["register"] = m(p("templates/register.gohtml", "templates/layout.gohtml"))
+	tmpl["about"] = m(p("templates/about.gohtml", "templates/layout.gohtml")) // Added about template
 }
